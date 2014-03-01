8fc4e5f21ea71791195e6bf6ed6eeabf8dcd6866
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2014-03-01 12:02:38 -0300

    Update feed.js

--- src/feed.js
+++ src/feed.js
@@ -34,7 +34,7 @@
         throw 'You need pass URL like parameter!';
       }
  
-      this.context = params.context || window;
+      this.context = params.context || root;
       this.limit = params.limit || 10;
       this.callback = params.callback || function() {};
     }.call(that, params);
